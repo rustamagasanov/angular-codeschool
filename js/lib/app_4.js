389de274ee5f978527d3d695a8cda56e2b1fffe7
Rustam Gasanov <rustamagasanov@gmail.com>
2015-08-26 21:32:54 +0200

    Added example 4

---
+++ js/lib/app_4.js
@@ -0,0 +1,43 @@
+(function(){
+  var app = angular.module('store', []);
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2,
+      description: '. . .',
+      canPurchase: true,
+      soldOut: false,
+      images: [
+        {
+          full: 'img/dodecahedron-01-full.jpg',
+          thumb: 'img/dodecahedron-01-thumb.jpg'
+        },
+        {
+          full: 'img/dodecahedron-02-full.jpg',
+          thumb: 'img/dodecahedron-02-thumb.jpg'
+        },
+      ]
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: '. . .',
+      canPurchase: false,
+      soldOut: false,
+      images: [
+        {
+          full: 'img/pentagonal-01-full.jpg',
+          thumb: 'img/pentagonal-01-thumb.jpg'
+        },
+        {
+          full: 'img/pentagonal-02-full.jpg',
+          thumb: 'img/pentagonal-02-thumb.jpg'
+        },
+      ]
+    }
+  ];
+})();
